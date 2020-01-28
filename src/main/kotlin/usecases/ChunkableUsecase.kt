0bfd88fc0e02f1fb0894f8b3acf588b05529600c
Daisuke Miyamoto <dai.0304@gmail.com>
2020-01-28 16:27:06 +0900

    upgrade dependency

--- src/main/kotlin/usecases/ChunkableUsecase.kt
+++ src/main/kotlin/usecases/ChunkableUsecase.kt
@@ -3,6 +3,7 @@ package org.polycreo.presentation.usecases
 import java.io.Serializable
 import org.polycreo.chunkrequests.Chunkable
 import org.polycreo.chunks.Chunk
+import org.polycreo.chunks.ChunkFactory
 import org.polycreo.services.ChunkableService
 import org.springframework.transaction.annotation.Transactional
 
@@ -10,8 +11,13 @@ interface ChunkableUsecase<E, ID>
     where ID : Serializable,
           ID : Comparable<ID> {
 
+    val chunkFactory: ChunkFactory
+
     val service: ChunkableService<E, ID>
 
     @Transactional(readOnly = true)
-    fun findAll(chunkable: Chunkable): Chunk<E> = service.findAll(chunkable)
+    fun findAll(chunkable: Chunkable): Chunk<E> {
+        val found = service.findAll(chunkable)
+        return chunkFactory.createChunk(found, chunkable)
+    }
 }
